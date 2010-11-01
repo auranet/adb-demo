29b9201d8e5cd843eadb755bcef56c7231b32d27
bretweinraub <bret@ckuru.com>
2010-11-01 02:02:17 +0000

    working bar graph generator

---
+++ vendor/plugins/aura_visualize/lib/meteor/widget/bar_graph/spec.rb
@@ -0,0 +1,52 @@
+module Meteor
+  module Widget
+    module BarGraph
+      # You've generated a new Meteor Widget!
+      #
+      # Your new widget can be rendered with the following:
+      #
+      # <%= render_meteor_widget(::Meteor::Widget::AuraVisualize::Spec.new(...) -%>
+      #   
+      # This renders its partial, vendor/plugins/aura_visualize/templates/aura_visualize/_render.rhtml.
+      #
+      # In your partial you can get a hold of your spec object with a local call 'spec'.
+
+      class Spec < ::Meteor::PluginSpecBase
+
+        include MeteorWidgetPlugin
+        
+        attr_accessor :columns,:data,:page_div
+
+        def initialize(options={},&block)
+          super(options,&block)
+          [:columns,:data,:page_div].each do |field|
+            raise "set :#{field} in #{self.class}.#{current_method}; this is required" unless self.send(field)
+          end
+        end
+
+        def column_data(record,column_number)
+          ret = ''
+          column=columns[column_number]
+          
+          ret += "'" if column[:type] == :string
+          
+          ret += record.send(column[:field]).to_s
+          
+          ret += "'" if column[:type] == :string
+
+          ret
+        end
+        
+        def max
+          raise "you must override this function"
+        end
+
+        def type_format(type,data)
+          extras=(type == :string ? "'" : '') 
+          "#{extras}#{data}#{extras}"
+        end
+
+      end
+    end
+  end
+end
